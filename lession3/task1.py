f258d6805dc53152e166705fc4ac3541f04b3190
alexabolot1 <alexabolot@gmail.com>
2021-01-22 15:02:06 +0300

    Решил первую задачу 3-го урока

---
+++ lession3/task1.py
@@ -0,0 +1,25 @@
+""""
+Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
+Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
+"""
+
+
+def my_func(a, b):
+    if b != 0:
+        if a / b % 2 != 0:
+            return round(a / b, 2)
+        else:
+            return a // b
+    else:
+        return
+
+
+while True:
+    number_one = int(input('Введите делимое '))
+    number_two = int(input('Введите делитель '))
+    if my_func(number_one, number_two) is None:
+        print('На 0 делить нельзя! Давайте попробуем еще раз :)')
+        continue
+    else:
+        print(f'Результат деления - {my_func(number_one, number_two)}')
+        break
